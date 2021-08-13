cc57a5f935c24182fd9d26620755fe560d86cee3
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-08-13 13:42:48 -0400

    make countdown timezone independent

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@
 	--------------------------------------*/
 	if (jQuery("[data-countdown]").length > 0) {
 		$('[data-countdown]').each(function () {
-			var $this = $(this), finalDate = $(this).data('countdown');
+			var $this = $(this);
+			var finalDate = 1633996800000; // 12 Oct 2021 00:00:00 GMT
 			$this.countdown(finalDate, function (event) {
 				$this.html(event.strftime('<span class="cdown days"><p>Days</p> <span class="time-count">%-D</span></span> <span class="cdown hour"><p>Hours</p> <span class="time-count">%-H</span></span> <span class="cdown minutes"><p>Minutes</p> <span class="time-count">%M</span></span> '));
 			});
