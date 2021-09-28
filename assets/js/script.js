dcab5e0fa4778a636a5bf197b458c336c5d568af
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-27 23:48:24 -0400

    countdown to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,44 @@
 	if (jQuery("[data-countdown]").length > 0) {
 		$('[data-countdown]').each(function () {
 			var $this = $(this);
+
 			var finalDate = 1633996800000; // 12 Oct 2021 00:00:00 GMT
-			$this.countdown(finalDate, function (event) {
-				$this.html(event.strftime('<span class="cdown days"><p>Days</p> <span class="time-count">%-D</span></span> <span class="cdown hour"><p>Hours</p> <span class="time-count">%-H</span></span> <span class="cdown minutes"><p>Minutes</p> <span class="time-count">%M</span></span> '));
-			});
+			// TODO: dev, remove
+			finalDate = 1632797644244 + 1000*60*60;
+
+			const tick = function (event) {
+				const now = new Date().getTime();
+				const delta = finalDate-now;
+				const fmt = 
+					'<span class="cdown days"><p>Days</p> <span class="time-count">%-D</span></span> '+
+					'<span class="cdown hour"><p>Hours</p> <span class="time-count">%-H</span></span> '+
+					'<span class="cdown minutes"><p>Minutes</p> <span class="time-count">%M</span></span> ';
+				$this.html(event.strftime(fmt));
+				if (delta < 1000*60 && !done) {
+					timerDone();
+				}
+			};
+			const timerDone = () => {
+				done = true;
+				const ctaHTML = 
+				"<a href='https://app.rewilder.xyz/#' role='button'"+
+					"class='btn btn-theme'>"+
+					" Donate now! "
+				"</a>"+
+				"<h4>Fundraising started Oct 12, 2021.</h4>";
+				$('#hero-main').html(ctaHTML);
+			};
+
+			const countdown = $this.countdown(finalDate);
+			const now = new Date().getTime();
+			let done = false;
+			
+			if (now > finalDate) {
+				timerDone();
+			}
+
+			countdown.on('update.countdown', tick);
+			countdown.on('finished.countdown', tick);
 		});
 	};
 
@@ -26,8 +60,10 @@
 	
 	// faq auto-open
 	var hash = window.location.hash.substr(1);
-	$("#"+hash).children('.accordion-head')	.toggleClass("active").next().slideToggle()
-	.siblings().children('.accordion-head').removeClass('.active');
+	if (hash) {
+		$("#"+hash).children('.accordion-head')	.toggleClass("active").next().slideToggle()
+		.siblings().children('.accordion-head').removeClass('.active');
+	}
 
 })(jQuery);
 
