9e9037068ab71809a10aa66935e8d940f9eac999
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-28 17:03:34 -0400

    emotion cache

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import emotionCache from "../lib/emotion-cache";
+import createEmotionServer from "@emotion/server/create-instance";
+
+const { extractCritical } = createEmotionServer(emotionCache);
 
 export default class MyDocument extends Document {
+
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
+
   render() {
     // prettier-ignore
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" />
           {/* favicon */}
           <link rel="shortcut icon" href="/favicon.png" />
           
