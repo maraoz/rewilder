bf4e3fc03fbf51d529329d707f2986ca78f596f9
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2022-01-25 13:34:19 -0300

    simplify index (remove countdown logic)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,6 @@
 
     'use strict';
 
-	// countdown
-	if (jQuery("[data-countdown]").length > 0) {
-		$('[data-countdown]').each(function () {
-			var $this = $(this);
-
-			var finalDate = 1633996800000; // 12 Oct 2021 00:00:00 GMT
-
-			const tick = function (event) {
-				const now = new Date().getTime();
-				const delta = finalDate-now;
-				const fmt = 
-					'<span class="cdown days"><p>Days</p> <span class="time-count">%-D</span></span> '+
-					'<span class="cdown hour"><p>Hours</p> <span class="time-count">%-H</span></span> '+
-					'<span class="cdown minutes"><p>Minutes</p> <span class="time-count">%M</span></span> ';
-				$this.html(event.strftime(fmt));
-				if (delta < 1000*60 && !done) {
-					timerDone();
-				}
-			};
-			const timerDone = () => {
-				done = true;
-				const ctaHTML = 
-					"<a href='https://app.rewilder.xyz/#' role='button'"+
-						"class='btn btn-theme'>"+
-						"Donate now!"
-					"</a>";
-				const explainHTML = 
-				"<h4>Fundraising started Oct 12, 2021.</h4>";
-				$('#hero-main').html(ctaHTML);
-				$('#hero-main').after(explainHTML);
-			};
-
-			const countdown = $this.countdown(finalDate);
-			const now = new Date().getTime();
-			let done = false;
-			
-			if (now > finalDate) {
-				timerDone();
-			}
-
-			countdown.on('update.countdown', tick);
-			countdown.on('finished.countdown', tick);
-		});
-	};
-
-
 	// accordion
 	$(".single-accordion").click(function(e){
 
